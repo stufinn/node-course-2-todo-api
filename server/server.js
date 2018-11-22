f6dff66406f93e980f38d982b88f7a0543ec1013
stufinn <30563561+finst@users.noreply.github.com>
2018-11-22 11:27:00 -0600

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ var app = express();
 //with this, can send json data to our application
 app.use(bodyParser.json());
 
+//Route to POST todos
 app.post('/todos', (req,res) => {
     // console.log(req.body);
     var todo = new ToDo({
@@ -25,7 +26,18 @@ app.post('/todos', (req,res) => {
     }, (e) => {
         // console.log('Could not save todo', e);
         res.status(400).send(e);
-    })
+    });
+});
+
+//Route to GET todos (i.e. list all)
+
+app.get('/todos', (req,res) => {
+    ToDo.find().then( (todos) => {
+        //could just send todos as an array(?) but better to send as an object b/c you can add custom properties on later
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
 
 });
 
