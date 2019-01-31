ff01d65a8600f576d066e919bd4ce379b2d3f651
stufinn <30563561+finst@users.noreply.github.com>
2019-01-31 11:58:14 -0600

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -245,7 +245,7 @@ describe('DELETE /todos/:id', () => {
             expect(user).toExist();
             expect(user.password).toNotBe(password); // makes sure password was successfully hashed
             done();
-          });
+          }).catch((e) => done(e));
 
         });
     });
@@ -270,6 +270,58 @@ describe('DELETE /todos/:id', () => {
       })
       .expect(400)
       .end(done);
+
+    });
+  });
+
+  describe('POST /users/login', () => {
+
+    it('should log in a user and return auth token', (done) => {
+      var email = users[1].email;
+      var password = users[1].password;
+
+      request(app)
+        .post('/users/login')
+        .send({email, password})
+        .expect(200)
+        .expect((res) => {
+          expect(res.headers['x-auth']).toExist();
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          }).catch((e) => done(e));
+        });
+    });
+
+    it('should reject an invalid login', (done) => {
+
+      var email = users[1].email;
+      var password = 'wrongPassw0rd';
+
+      request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
 
     });
   });
