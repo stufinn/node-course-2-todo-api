c8ef140e7037d19d4e1776f1202c331f64f0596c
stufinn <30563561+finst@users.noreply.github.com>
2019-02-04 12:13:35 -0600

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -325,3 +325,26 @@ describe('DELETE /todos/:id', () => {
 
     });
   });
+
+  describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+
+      var token = users[0].tokens[0].token;
+
+      request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(200)
+      .end( (err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+        
+      });
+      
+    });
+  });
